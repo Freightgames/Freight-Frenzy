127fca57298d7c5543a7206b57a35104be6fb30e
Garrett <garrettcoleallen@gmail.com>
2025-03-28 20:42:51 -0400

    fixing bg color

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const billboardMessages = [
     { weight: 1.2, type: 'image', content: './billboard-images/wtt.jpg', analytics: 'whatthetruck-billboard'},
     { weight: 1.2, type: 'image', content: './billboard-images/scrm.png', analytics: 'scrm-billboard', bgColor: "#ffffff"},
     { weight: 1.2, type: 'image', content: './billboard-images/yms.png', analytics: 'yms-billboard', bgColor: "#f9e231"},
-    { weight: 1.2, type: 'image', content: './billboard-images/shimmi.jpg', analytics: 'shimmi-billboard'},
+    { weight: 1.2, type: 'image', content: './billboard-images/shimmi.jpg', analytics: 'shimmi-billboard', bgColor: "#ffffff"},
 ];
 
 // Add clock for delta time calculation
